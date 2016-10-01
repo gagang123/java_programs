187c319ad8288a810650c8fe105300be9b589f2f
Gagandeep Garg <gagandeepgarg6@gmail.com>
2016-10-01 18:35:54 -0300

    Add files via upload

---
+++ arrayleftrotation.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static int[] arrayLeftRotation(int[] a, int n, int k) {
+       
+        
+        for(int j=0; j<k; j++){
+            int temp = a[0];
+            for(int i=0; i<n; i++){
+             if(i==n-1){
+                a[n-1]=temp;
+                break;
+             }
+           a[i]=a[i+1];    
+        }
+     }
+       return a;
+       
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+      
+        int[] output = new int[n];
+        output = arrayLeftRotation(a, n, k);
+        for(int i = 0; i < n; i++)
+            System.out.print(output[i] + " ");
+      
+        System.out.println();
+      
+    }
+}
